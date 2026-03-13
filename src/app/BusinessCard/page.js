5ae51d12b24fb24ae649867ddee6da81728bc7e0
Antoine Moevus <2613844+antmoev@users.noreply.github.com>
2026-03-12 20:41:23 -0400

    add Exo1 seance 1

---
+++ src/app/BusinessCard/page.js
@@ -0,0 +1,11 @@
+const BusinessCard = () => {
+  return (
+    <div className="business-card">
+      <h2>Jane Doe</h2>
+      <p>Software Engineer</p>
+      <p>jane.doe@example.com</p>
+    </div>
+  );
+}
+
+export default BusinessCard;
