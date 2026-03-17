7712ad51d24abaeda82f8a2bb835d1294268576e
Antoine Moevus <2613844+antmoev@users.noreply.github.com>
2026-03-17 19:08:08 -0400

    Add 2 composants

--- src/app/BusinessCard/page.js
+++ src/app/BusinessCard/page.js
@@ -1,11 +1,19 @@
+import {ButtonRouge, Button as Banana} from "@/composants/Button";
+// import {Button as EasterButton} from "@/composants/easter/Button";
+
+
 const BusinessCard = () => {
   return (
     <div className="business-card">
       <h2>Jane Doe</h2>
       <p>Software Engineer</p>
       <p>jane.doe@example.com</p>
+
+      <ButtonRouge />
+      <Banana />
     </div>
   );
+
 }
 
 export default BusinessCard;
---
+++ src/composants/Button.jsx
@@ -0,0 +1,19 @@
+
+
+export function Button() {
+    return (
+        <div className="bg-blue-800">
+            <button>Click me</button>
+            <p>This is a button component.</p>
+        </div>
+    )
+}
+
+export function ButtonRouge() {
+    return (
+        <div className="bg-red-800">
+            <button>Click me</button>
+            <p>This is a red button component.</p>
+        </div>
+    )
+}
